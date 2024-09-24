82330d3bcf118ab60807591a3c400b0367a8bafc
bissaliev <bissaliev21@gmail.com>
2024-09-24 10:02:38 +0400

    Создал главную страницу

--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -5,7 +5,11 @@ class Menu(models.Model):
     name = models.CharField("Название", max_length=150)
     slug = models.SlugField("Слаг", max_length=150, unique=True)
     parent = models.ForeignKey(
-        "self", on_delete=models.CASCADE, related_name="children", blank=True
+        "self",
+        on_delete=models.CASCADE,
+        related_name="children",
+        blank=True,
+        null=True,
     )
 
     class Meta:
