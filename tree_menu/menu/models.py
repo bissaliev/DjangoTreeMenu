f510c7e4a36e3029fed6fc44104b1e3f7d310bbe
bissaliev <bissaliev21@gmail.com>
2024-09-24 09:51:20 +0400

    Создал модель 'Menu'

---
+++ tree_menu/menu/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField("Название", max_length=150)
+    slug = models.SlugField("Слаг", max_length=150, unique=True)
+    parent = models.ForeignKey(
+        "self", on_delete=models.CASCADE, related_name="children", blank=True
+    )
+
+    class Meta:
+        verbose_name = "Меню"
+        verbose_name_plural = "Меню"
+
+    def __str__(self):
+        return self.name
