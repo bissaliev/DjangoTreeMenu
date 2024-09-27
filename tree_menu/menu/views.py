fa5c54dbec0cfd2b968535fa103cfbcaad158efa
bissaliev <bissaliev21@gmail.com>
2024-09-27 09:28:33 +0400

    Создал функции-представления index and menu

--- tree_menu/menu/views.py
+++ tree_menu/menu/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from menu.models import Menu
 
-# Create your views here.
+
+def index(request):
+    menu_items = Menu.objects.filter(parent=None)
+    title = "Главная страница"
+    return render(
+        request, "menu/index.html", {"menu_items": menu_items, "title": title}
+    )
+
+
+def menu(request, menu_slug):
+    current_menu = get_object_or_404(Menu, slug=menu_slug)
+    return render(request, "menu/menu.html", {"current_menu": current_menu})
